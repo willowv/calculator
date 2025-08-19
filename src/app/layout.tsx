6781cd13eb1f9ebb4bc5a107c1a7089fd39c341e
willowv <eps.willow@gmail.com>
2025-08-19 09:44:00 -0700

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+
+const geistSans = Geist({
+    variable: '--font-geist-sans',
+    subsets: ['latin']
+})
+
+const geistMono = Geist_Mono({
+    variable: '--font-geist-mono',
+    subsets: ['latin']
+})
+
+export const metadata: Metadata = {
+    title: 'Calculator Assessment',
+    description: 'Frontend coding assessment for HiHello'
+}
+
+export default function RootLayout({
+    children
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+                    <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+                        {children}
+                    </main>
+                </div>
+            </body>
+        </html>
+    )
+}
